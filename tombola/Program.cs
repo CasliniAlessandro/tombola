94089a1c3e749dd2d399e50b1164e79d667d388d
CasliniAlessandro <121171563+CasliniAlessandro@users.noreply.github.com>
2023-01-09 16:28:37 +0100

    Update Program.cs
    chi vince e colore tabellone

--- tombola/Program.cs
+++ tombola/Program.cs
@@ -1,49 +1,66 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-
 class Program
 {
     public static void Main(string[] args)
     {
-             //dichiarazioni 
-        
+        //dichiarazioni 
+  
         Console.WriteLine("TOMBOLA");
 
         Console.WriteLine("TABELLONE");
 
         int variabile;
         variabile = 1;
-        int estrazione;
         Random r = new Random();
         int[,] mat = new int[9,10];
         int[,] mat2 = new int[3,5];
-        
+        int numestrazione=1;
+
+
         //CARICAMENTO DELLA MATRICE TABELLONE
+
         for (int i = 0; i < 9; i++)
         {
             for (int j = 0; j < 10; j++)
             {
                 
                 mat[i, j] = variabile++ ;
+
+                if (mat[i, j] == numestrazione)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
                 
-               
             }
             Console.WriteLine();
+
 
             for (int k = 0; k < 10; k++)
             {
                 Console.Write(mat[i,k]+" ") ;
+
+                
             }
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
 
         // SCHEDA GIOCATORE 1 
+        Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine();
         Console.WriteLine("CARTELLA GIOCATORE 1");
 
@@ -53,9 +70,12 @@ class Program
             
             for (int j = 0; j<5; j++)
             {
-
                 mat2[i, j] = r.Next(1,90);
 
+                if (mat2[i, j] == numestrazione)
+                {
+                    Console.WriteLine("Il vincitore è il giocatore 1");
+                }
 
             }
             Console.WriteLine();
@@ -65,8 +85,8 @@ class Program
             }
             Console.WriteLine();
         }
-
-        // SCHEDA GIOCATORE 2 
+        //SCHEDA GIOCATORE 2 
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine();
         Console.WriteLine("CARTELLA GIOCATORE 2");
 
@@ -75,29 +95,43 @@ class Program
         {
             for (int j = 0; j < 5; j++)
             {
-
+             
                 mat2[i, j] = r.Next(1,90);
 
+                if (mat2[i, j] == numestrazione)
+                {
+                    Console.WriteLine("Il vincitore è il giocatore 2") ;
+                }
 
             }
             Console.WriteLine();
-
             for (int k = 0; k < 5; k++)
             {
                 Console.Write(mat2[i, k] + " ");
             }
             Console.WriteLine();
         }
-        
+        Console.WriteLine();
+
         //ESTRAZIONE NUMERI 
-        for (int i = 0; i < 91; i++)
+        Console.ForegroundColor = ConsoleColor.White;
+
+        for (int i = 1; i < 91; i++)
         {
             Console.WriteLine("Numero estratto: ");
-            Console.WriteLine(estrazione = r.Next(1, 90));
+            Console.WriteLine(numestrazione = r.Next(1, 90));
             Console.WriteLine("Premere un tasto per continuare:"); 
             Console.ReadLine();
+
         }
 
 
+
+         
+
+
+
+
+
     }
-}
+
